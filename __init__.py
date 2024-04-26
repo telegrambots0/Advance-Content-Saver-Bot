ada4f3556644877c01527452af7db7811d743503
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-04-26 18:29:31 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,12 +15,12 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None)
+API_ID = "28619451"config("API_ID", default=None, cast=int)
+API_HASH = "e41e306cd0e0d0be11ec78c281f0b628" config("API_HASH", default=None)
+BOT_TOKEN = "7072495432:AAHMFM3ejs_BZ0joIBq1_R0KOO1y3YNhFK8" config("BOT_TOKEN", default=None)
+SESSION = "BAGobe0AjWCWbnYPtQGB_8EGMSpkC10XMy-RN4uM0bGPNuU6CjTNbhbeIeKnUVZUdnwPJnI2W_W50Q2Bh0qYHACxh3F0dA2Awsjnu03cNzZZoS27kZ8Y-smM3Tt1Hx95ll0zGyrLB36ziigKOkVNrdYQM_SWNsITgLpPRWV0UnOvj_R5EdOKFByzywhfKThjJB3-i39SYY9rRTEqzMwfFCFTRojARQ13KC8SmZ14dpSpLA17bytX8lwoawmG6eVD_ukfmk76w45b6O3jbqnm7LdFC56v_77FOqU03_mVdp5dSq0HMttjX7_K4whCEbVbPDJblMm_GdM9cRyk7GBdkUBobwEQCQAAAAE7BEmYAA" config("SESSION", default=None)
+FORCESUB = "SmexyStore" config("FORCESUB", default=None)
+AUTH = "5285104024" config("AUTH", default=None)
 SUDO_USERS = []
 
 if len(AUTH) != 0:
